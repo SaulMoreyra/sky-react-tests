a4c0f6c8012d94a45e0348a04b30e697986b8998
SaulMoreyra <armhacam21@gmail.com>
2022-03-09 19:32:28 -0600

    feat: function for return rgba from hex added

--- src/utils/StyleUtils.ts
+++ src/utils/StyleUtils.ts
@@ -2,6 +2,15 @@ const BASE_SPACING = 8;
 export const spacing = (...spaces: number[]) =>
   spaces.map((space) => `${space * BASE_SPACING}px`).join(" ");
 
+export const fade = (hex: string, alpha = 1) => {
+  if (!/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) throw new Error("Bad Hex Color");
+  const r = parseInt(hex.slice(1, 3), 16),
+    g = parseInt(hex.slice(3, 5), 16),
+    b = parseInt(hex.slice(5, 7), 16);
+
+  return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+};
+
 export const breakpoints = {
   in: "1px",
   sm: "576px",
