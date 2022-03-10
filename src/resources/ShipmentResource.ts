e76fccfb42c1450831addbfc44ac5fee3d2a2fe0
SaulMoreyra <armhacam21@gmail.com>
2022-03-09 19:33:32 -0600

    feat: json base with shipment info

---
+++ src/resources/ShipmentResource.ts
@@ -0,0 +1,44 @@
+import { ShipmentCreate } from "interfaces/ShipmentCreate";
+
+export const shipment = <ShipmentCreate>{
+  address_from: {
+    province: "Ciudad de México",
+    city: "Azcapotzalco",
+    name: "Jose Fernando",
+    zip: "02900",
+    country: "MX",
+    address1: "Av. Principal #234",
+    company: "skydropx",
+    address2: "Centro",
+    phone: "5555555555",
+    email: "skydropx@email.com",
+  },
+  parcels: [
+    {
+      weight: 3,
+      distance_unit: "CM",
+      mass_unit: "KG",
+      height: 10,
+      width: 10,
+      length: 10,
+    },
+  ],
+  address_to: {
+    province: "Jalisco",
+    city: "Guadalajara",
+    name: "Jorge Fernández",
+    zip: "44100",
+    country: "MX",
+    address1: " Av. Lázaro Cárdenas #234",
+    company: "-",
+    address2: "Americana",
+    phone: "5555555555",
+    email: "ejemplo@skydropx.com",
+    reference: "Frente a tienda de abarro",
+    contents: "Hola",
+  },
+  consignment_note_class_code: "53131600",
+  consignment_note_packaging_code: "1H1",
+};
+
+export default { shipment };
