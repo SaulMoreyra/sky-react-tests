ff0c21b7b30895048ff37386afb1db82a31b242b
SaulMoreyra <armhacam21@gmail.com>
2022-03-09 19:34:48 -0600

    feat: poppins font added, jest config modified

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   moduleDirectories: ["node_modules", "src"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
 };
--- src/theme/DefaultTheme.ts
+++ src/theme/DefaultTheme.ts
@@ -1,14 +1,20 @@
-import { breakpoints, spacing } from "utils/StyleUtils";
+import { breakpoints, spacing, fade } from "utils/StyleUtils";
 
 const theme = {
   breakpoints,
   spacing,
+  fade,
   black: "#000000",
-  ligthBlack: "#2e2e2e",
+  lightBlack: "#2e2e2e",
   white: "#FFFFFF",
   grey: "#50514F",
-  ligthGrey: "#F2F2F2",
+  lightGrey: "#F2F2F2",
+  primaryLight: "#7E6BEC",
   primary: "#4e34e1",
+  primaryDark: "#3C1FE0",
+  error: "#f44336",
+  success: "#28a745",
+  warning: "#ffc107",
 };
 
 export type Theme = typeof theme;
